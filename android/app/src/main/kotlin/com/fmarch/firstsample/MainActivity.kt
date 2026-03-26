d89b40cf9e96f92d7b8c4ccf80f7b01755705798
welkincedev <welkflutter@gmail.com>
2026-03-26 13:07:01 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/fmarch/firstsample/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fmarch.firstsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'firstpage.dart';
+
+void main() =>
+    runApp(MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Firstpage()));
